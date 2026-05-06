20aabddd16f115a99bf532d16ffd46d0c11e6851
Reneja-Martin <renejamartin@hotmail.com>
2026-05-06 15:21:53 -0400

    Update healthcare-analysis.sql
    Update healthcare SQL queries

--- Healthcare-SQL-Project/healthcare-analysis.sql
+++ Healthcare-SQL-Project/healthcare-analysis.sql
@@ -1,24 +1,40 @@
 -- Total appointments
 SELECT COUNT(*) AS total_appointments
-FROM healthcare_appointments;
+FROM appointments;
 
 -- No-show count by department
 SELECT department,
        COUNT(*) AS no_show_count
-FROM healthcare_appointments
-WHERE attendance_status = 'No Show'
+FROM appointments
+WHERE appointment_status = 'No-Show'
 GROUP BY department
 ORDER BY no_show_count DESC;
 
 -- Average waiting time by appointment type
 SELECT appointment_type,
        AVG(waiting_time_minutes) AS avg_wait_time
-FROM healthcare_appointments
+FROM appointments
 GROUP BY appointment_type;
 
 -- Appointment count by department
 SELECT department,
        COUNT(*) AS total_appointments
-FROM healthcare_appointments
+FROM appointments
 GROUP BY department
 ORDER BY total_appointments DESC;
+
+
+-- Completed appointments by appointment type
+SELECT appointment_type,
+       COUNT(*) AS completed_appointments
+FROM appointments
+WHERE appointment_status = 'Completed'
+GROUP BY appointment_type
+ORDER BY completed_appointments DESC;
+
+
+SELECT department,
+       AVG(waiting_time_minutes) AS avg_waiting_time
+FROM appointments
+GROUP BY department
+ORDER BY avg_waiting_time DESC;
